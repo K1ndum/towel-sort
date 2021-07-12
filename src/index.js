6210e5e5c99ac4c9f4c2c55b4de3f553d44ddf00
K1ndum <kindum98@gmail.com>
2021-07-12 23:16:41 +0300

    fix
    add sort

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+  if (matrix == undefined) {
+    return [];
+  } else {
   let arr = [];
   let leng = 0;
   for (let i = 0; i < 10; i++) {
       if (matrix[i] !== undefined) {
-          leng ++;
+          leng ++
       } else break;
   }
     for (let i = 0; i < leng; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
-        arr.push(matrix[i][j]);
+          if ( i % 2 == 0) {
+              arr.push(matrix[i][j]);
+          } else {
+              arr.push(matrix[i][matrix[i].length - j -1])
+          }
       }
     }
   return arr;
+  }
   
 }
